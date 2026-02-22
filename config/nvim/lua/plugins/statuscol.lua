f3536649101001f8f47a2113da18b85e84a862d0
Gabriel Barbosa de Oliveira <gabrieloliveira00@outlook.com>
2026-02-22 19:16:57 -0300

    fix: Remove auto from statuscol sign

--- config/nvim/lua/plugins/statuscol.lua
+++ config/nvim/lua/plugins/statuscol.lua
@@ -20,7 +20,6 @@ return {
                     sign = {
                         namespace = { ".*" },
                         name = { ".*" },
-                        auto = true,
                     },
                 },
                 {
